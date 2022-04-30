612ffaa46b39b36aa126ad64bcf1dbcb37fc70dd
Yiğit Can Başalma <yigit.basalma@gmail.com>
2022-04-30 16:36:16 +0300

    New deployment type

--- vars/lib_deployController.groovy
+++ vars/lib_deployController.groovy
@@ -1,22 +1,28 @@
 def call(Map config) {
     config.b_config.deploy.each { it ->
         def image = sh(
-            script: "${config.script_base}/infos.py --project ${config.b_config.project.name} --workspace ${WORKSPACE} -m get_container_version --container-name ${it.name}",
+            script: "${config.script_base}/meta.py -p ${config.b_config.project.name} -a ${it.name} -e \$(cat /etc/stack-env) --provider ${config.meta_provider} get --image-name",
             returnStdout: true
         ).trim()
 
-        "${it.type}"(config, image, it.repo, it.path, it.name)
+        "${it.type}"(config, image, it.repo, it.path, it.name, config.b_config.project.name)
+
+        sh """
+        ${config.script_base}/meta.py -p ${config.b_config.project.name} -a ${it.name} -e \$(cat /etc/stack-env) --provider ${config.meta_provider} set --image-id ${image}
+        """
     }
 }
 
-def argocd(Map config, String image, String repo, String path, String appName) {
+def argocd(Map config, String image, String repo, String path, String appName, String projectName) {
     // Change image version on argocd repo and push
     sh """
     ${config.script_base}/argocd.py --image "${image}" -r ${repo} --application-path ${path}/${config.environment}
     """
+}
 
-    // Save image version for environment
+def lambda(Map config, String image, String repo, String path, String appName, String projectName) {
+    // Change image version on argocd repo and push
     sh """
-    ${config.script_base}/infos.py --project ${config.b_config.project.name} --workspace ${WORKSPACE} -m save_env_container_map --container-environment-image-map "${config.environment};${image}" --app-name ${appName}
+    aws lambda invoke --function-name promote-image --payload '{"project-identifier": "${projectName}", "app-identifier": "${appName}", "environment": "\$(cat /etc/stack-env)", "image": "${image}"}' lambda-out.json
     """
 }
